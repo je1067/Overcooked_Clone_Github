b4aad17167f11758a45c9f28c47bae5bc2661020
Jeremy <je1067@nyu.edu>
2017-08-03 14:54:58 -0400

    Simple Timer System + Preliminary Tags

---
+++ Assets/Scripts/TimerText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerText : MonoBehaviour {
+    public Text timeLeftText;
+    public GameObject gameOverText;
+    public string timeText;
+    public float timeLeft = 60.0f;
+    
+    
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        timeLeft -= Time.deltaTime;
+
+        //timeText = (timeLeft);
+
+        timeLeftText.text = (timeLeft.ToString("F0"));
+        if (timeLeft <= 0.0f){
+            // timeLeftText.SetActive(false);
+            timeLeft = 0.0f;
+            gameOverText.SetActive(true);
+        }
+	}
+}
