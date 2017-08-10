792972551a9a9c6ad03a4ccc5d568eb5a0953a6f
Unknown <tonicwater720@gmail.com>
2017-08-10 15:11:17 -0400

    Fixed spawning and grabbing

--- Assets/Scripts/Spawn_Point.cs
+++ Assets/Scripts/Spawn_Point.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Spawn_Point : MonoBehaviour {
 
+
+	// ADD TIMER 
 	public GameObject objectToSpawn;
 	GameObject spawnedObject;
 	public Transform player;
 	public Transform player2;
+	public Players_Move play1, play2;
 	public bool canSpawn;
 	public bool canSpawn2;
+	public bool alreadyHolding = false;
+	float cooldown = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,43 +23,58 @@ public class Spawn_Point : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Vector3.Distance (player.position, transform.position) < 6f) {
+		cooldown += Time.deltaTime;
+
+		if (Vector3.Distance (player.position, transform.position) < 0.5f) {
 
 
 
-			if (Input.GetKeyDown (KeyCode.E) && canSpawn) {
+			if (Input.GetKeyDown (KeyCode.E) && canSpawn && cooldown >= 1f && !alreadyHolding) {
 
 				spawnedObject = Instantiate (objectToSpawn, player.position, transform.rotation) as GameObject;
 				canSpawn = false;
 				spawnedObject.transform.parent = player.transform;
 				spawnedObject.GetComponent<Rigidbody> ().isKinematic = true;
 				spawnedObject.GetComponent<Pick_UP_test_Omar> ().dropped = false;
+				cooldown = 0;
 
 
 			}
-		} else if (Vector3.Distance (player.position, transform.position) > 5f) {
+		} else if (Vector3.Distance (player.position, transform.position) > 1.5f) {
 			canSpawn = true;
 		}
 
 
-		if (Vector3.Distance (player2.position, transform.position) < 6f) {
+		if (Vector3.Distance (player2.position, transform.position) < 0.5f) {
 
 
 
-			if (Input.GetKeyDown (KeyCode.O) && canSpawn2) {
+			if (Input.GetKeyDown (KeyCode.O) && canSpawn2 && cooldown >= 1f && !alreadyHolding) {
 
 				spawnedObject = Instantiate (objectToSpawn, player2.position, transform.rotation) as GameObject;
-				canSpawn = false;
+				canSpawn2 = false;
 				spawnedObject.transform.parent = player2.transform;
 				spawnedObject.GetComponent<Rigidbody> ().isKinematic = true;
 				spawnedObject.GetComponent<Pick_UP_test_Omar> ().dropped2 = false;
+				cooldown = 0;
 
 
 			}
-		} else if (Vector3.Distance (player2.position, transform.position) > 5f) {
+		} else if (Vector3.Distance (player2.position, transform.position) > 1.5f) {
 			canSpawn2 = true;
 		}
 
+		
+	}
+
+	void OnTriggerStay(Collider col){
+
+		alreadyHolding = true;
+		
+	}
+
+	void OnTriggerExit(Collider col){
+		alreadyHolding = false;
 		
 	}
 }
